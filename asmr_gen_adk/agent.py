2c89b63814c162ff19418682ca32f1f5431345ec
nozoyoshida <ycuriosity011698@gmail.com>
2025-09-04 10:19:58 +0000

    update

--- asmr_gen_adk/agent.py
+++ asmr_gen_adk/agent.py
@@ -1,11 +1,11 @@
-from google.adk.agents import LlmAgent
 from google.adk.agents import SequentialAgent
 from .agents.script_agent import script_agent
 from .agents.tts_agent import tts_agent
+from .agents.spatial_plan_agent import spatial_plan_agent
+from .agents.asmr_agent import asmr_agent
 
 root_agent = SequentialAgent(
     name="asmr_gen_seq",
-    description="Generate ASMR script then synthesize TTS in strict order.",
-    # sub_agents=[script_agent, tts_agent],
-    sub_agents=[spacial_plan_agent, asmr_agent],
+    description="Generate a full ASMR experience from a situation.",
+    sub_agents=[script_agent, tts_agent, spatial_plan_agent, asmr_agent],
 )
